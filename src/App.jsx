1ffa31fc826c9b68e673c4624e990f5cae86fbd7
aleks Manevski <oceboosting@gmail.com>
2023-06-12 05:00:27 +0930

    f3

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ function App() {
     <>
       <div>
         <Navbar />
-        <Skills />
         <Logout />
         <Login />
         <Registration />
--- src/skills/Skills.jsx
+++ src/skills/Skills.jsx
@@ -40,18 +40,18 @@ const Skills = () => {
               <h4 className="front-end-title skills-headings">
                 FRONT-END DEVELOPMENT
               </h4>
-              <li className="skills-lis">JavaScript</li>
+              <li className="skills-lis">JavaScript / React / Bootstrap</li>
+              <li className="skills-lis">HTML5</li>
               <li className="skills-lis">CSS</li>
-              <li className="skills-lis">React</li>
-              <li className="skills-lis">Bootstrap</li>
 
               <h4 className="skills-headings">BACK-END DEVELOPMENT</h4>
-              <li className="skills-lis">Node</li>
               <li className="skills-lis">Express</li>
-              <li className="skills-lis">Azure</li>
+              <li className="skills-lis">Node</li>
 
               <h4 className="skills-headings">DATABASE INTEGRATION</h4>
               <li className="skills-lis">MonboDB</li>
+              <li className="skills-lis">Firebase</li>
+              <li className="skills-lis">Azure</li>
               <li className="skills-lis">SQL</li>
             </ul>
           </div>
