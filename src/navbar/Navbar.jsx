4f04ba4418072963796cf2d4ee78189907f8540d
aleks Manevski <oceboosting@gmail.com>
2023-06-12 06:54:09 +0930

    efwfwef

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -35,12 +35,6 @@ function Navbar() {
               <a href="/home">Home</a>
             </li>
             <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/skills">My Skills</a>
-            </li>
-            <li>
               <a href="/contact">Contact</a>
             </li>
             <li>
