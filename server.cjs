633f0d3c76388706919f42270036cd3dc30ebfad
heyaleks21 <oceboosting@gmail.com>
2023-06-12 21:38:47 +0930

    s

--- server.cjs
+++ server.cjs
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-
+var port = process.env.PORT || 3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
