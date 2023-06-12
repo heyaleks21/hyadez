1ed1b20840b9a22027e393332ccb4f91062ba059
aleks Manevski <oceboosting@gmail.com>
2023-06-12 21:07:42 +0930

    123123

--- server.cjs
+++ server.cjs
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
+app.listen(3000, () => console.log("Server Running"));
 
 const contactEmail = nodemailer.createTransport({
   service: "gmail",
@@ -19,7 +19,7 @@ const contactEmail = nodemailer.createTransport({
   secure: true,
 });
 
-await contactEmail.verify((error) => {
+contactEmail.verify((error) => {
   if (error) {
     console.log(error);
   } else {
@@ -27,7 +27,7 @@ await contactEmail.verify((error) => {
   }
 });
 
-await router.post("/contact", (req, res) => {
+router.post("/contact", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const message = req.body.message;
