427302a2813e1306acbbb0be12bff266af5e618b
heyaleks21 <oceboosting@gmail.com>
2023-06-26 22:38:20 +0930

    21

--- src/contact-form/Contact.jsx
+++ src/contact-form/Contact.jsx
@@ -1,14 +1,44 @@
 import { useState } from "react";
 import "./contact.css";
-import Navbar from "../navbar/Navbar";
 import { useForm, ValidationError } from "@formspree/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { fa1, fa2, fa3, fa4 } from "@fortawesome/free-solid-svg-icons";
 
 const Contact = () => {
+  const [formSent, setFormSent] = useState("Send");
   const [state, handleSubmit] = useForm("meqbjadw");
   if (state.succeeded) {
-    return <p>Message sent! We will be in touch.</p>;
+    return (
+      <div className="firstbox">
+        <div className="secondbox boxbox">
+          <div className="form">
+            <form className="submit-form form-body">
+              <h3 className="heading1">Message delivered!</h3>
+              <h2 className="heading1">What happens next</h2>
+              <div className="happenscontents">
+                <div className="contactindividuals">
+                  <FontAwesomeIcon icon={fa1} /> A member of our team reaches
+                  out within a few hours to organise a FREE consultation.
+                </div>
+                <div className="contactindividuals">
+                  <FontAwesomeIcon icon={fa2} /> Meanwhile, we sign an NDA to
+                  ensure the privacy and protection of your idea.
+                </div>
+                <div className="contactindividuals">
+                  <FontAwesomeIcon icon={fa3} /> We will conduct a FREE
+                  consultation to understand your business requirements and will
+                  answer any questions you may have.
+                </div>
+                <div className="contactindividuals">
+                  <FontAwesomeIcon icon={fa4} /> Our expert strategist presents
+                  project estimates and approximate timelines.
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <>
@@ -57,13 +87,8 @@ const Contact = () => {
                 id="message"
               />
               <div className="footer footer2">
-                <button
-                  className="btn"
-                  type="submit"
-                  name="name"
-                  disabled={state.submitting}
-                >
-                  Send
+                <button className="btn" type="submit" name="name">
+                  {formSent}
                 </button>
               </div>
             </form>
@@ -72,11 +97,7 @@ const Contact = () => {
 
         <div className="secondbox boxbox">
           <div className="form">
-            <form
-              onSubmit={handleSubmit}
-              className="submit-form form-body"
-              type="submit"
-            >
+            <form className="submit-form form-body">
               <h2 className="heading1">What happens next</h2>
               <div className="happenscontents">
                 <div className="contactindividuals">
