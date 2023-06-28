aa074034fed365879aeea898d9dad443dd32a974
heyaleks21 <oceboosting@gmail.com>
2023-06-28 17:37:22 +0930

    21

--- src/services/Services.jsx
+++ src/services/Services.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import "./services.css";
 
+import img1 from "../assets/services/graphic-design.png";
+import img2 from "../assets/services/keychain.png";
+import img3 from "../assets/services/database.png";
+import img4 from "../assets/services/seo.png";
+import img5 from "../assets/services/analytics.png";
+import img6 from "../assets/services/creative.png";
+import img7 from "../assets/services/social-media.png";
+import img8 from "../assets/services/photography.png";
+
 const Services = () => {
-  const [isShown1, setIsShown1] = useState("Web Design");
-  const [isShown2, setIsShown2] = useState("Branding");
-  const [isShown3, setIsShown3] = useState("Hosting");
-  const [isShown4, setIsShown4] = useState("SEO");
-  const [isShown5, setIsShown5] = useState("Analytics");
-  const [isShown6, setIsShown6] = useState("Content Creation");
-  const [isShown7, setIsShown7] = useState("Marketing");
-  const [isShown8, setIsShown8] = useState("Product Photography");
+  const [isShown1, setIsShown1] = useState("");
+  const [isShown2, setIsShown2] = useState("");
+  const [isShown3, setIsShown3] = useState("");
+  const [isShown4, setIsShown4] = useState("");
+  const [isShown5, setIsShown5] = useState("");
+  const [isShown6, setIsShown6] = useState("");
+  const [isShown7, setIsShown7] = useState("");
+  const [isShown8, setIsShown8] = useState("");
 
   return (
     <>
@@ -22,9 +31,10 @@ const Services = () => {
                 "Stunning and intuitive websites tailored to your unique brand and audience."
               )
             }
-            onMouseLeave={() => setIsShown1("Web Design")}
+            onMouseLeave={() => setIsShown1("")}
           >
             {isShown1 && <div className="cards">{isShown1}</div>}
+            <img className="tt22" src={img1}></img>
           </div>
           <div
             className="services-boxes"
@@ -33,8 +43,9 @@ const Services = () => {
                 "Create a memorable brand identity that reflects your values and resonates with your target market."
               )
             }
-            onMouseLeave={() => setIsShown2("Branding")}
+            onMouseLeave={() => setIsShown2("")}
           >
+            <img className="tt22" src={img2}></img>
             {isShown2 && <div className="cards">{isShown2}</div>}
           </div>
           <div
@@ -44,8 +55,9 @@ const Services = () => {
                 "Reliable and secure hosting solutions to keep your website running smoothly and efficiently."
               )
             }
-            onMouseLeave={() => setIsShown3("Hosting")}
+            onMouseLeave={() => setIsShown3("")}
           >
+            <img className="tt22" src={img3}></img>
             {isShown3 && <div className="cards">{isShown3}</div>}
           </div>
           <div
@@ -55,8 +67,9 @@ const Services = () => {
                 "Boost your online visibility and drive organic traffic with proven search engine optimization strategies."
               )
             }
-            onMouseLeave={() => setIsShown4("SEO")}
+            onMouseLeave={() => setIsShown4("")}
           >
+            <img className="tt22" src={img4}></img>
             {isShown4 && <div className="cards">{isShown4}</div>}
           </div>
           <div
@@ -66,8 +79,9 @@ const Services = () => {
                 "Gain valuable insights into your website's performance and user behavior to make data-driven decisions."
               )
             }
-            onMouseLeave={() => setIsShown5("Analytics")}
+            onMouseLeave={() => setIsShown5("")}
           >
+            <img className="tt22" src={img5}></img>
             {isShown5 && <div className="cards">{isShown5}</div>}
           </div>
           <div
@@ -77,8 +91,9 @@ const Services = () => {
                 "Engaging and compelling content that captivates your audience and drives conversions."
               )
             }
-            onMouseLeave={() => setIsShown6("Content Creation")}
+            onMouseLeave={() => setIsShown6("")}
           >
+            <img className="tt22" src={img6}></img>
             {isShown6 && <div className="cards">{isShown6}</div>}
           </div>
           <div
@@ -88,8 +103,9 @@ const Services = () => {
                 "Strategic digital marketing campaigns to amplify your online presence and reach your target customers."
               )
             }
-            onMouseLeave={() => setIsShown7("Marketing")}
+            onMouseLeave={() => setIsShown7("")}
           >
+            <img className="tt22" src={img7}></img>
             {isShown7 && <div className="cards">{isShown7}</div>}
           </div>
           <div
@@ -99,8 +115,9 @@ const Services = () => {
                 "High-quality visuals that showcase your products in the best light, enticing customers to make a purchase."
               )
             }
-            onMouseLeave={() => setIsShown8("Product Photography")}
+            onMouseLeave={() => setIsShown8("")}
           >
+            <img className="tt22" src={img8}></img>
             {isShown8 && <div className="cards">{isShown8}</div>}
           </div>
         </div>
